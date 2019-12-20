9772c992a254eca10033377c5b2c369f1d1908f3
kaixinjiuhao5 <58070410+kaixinjiuhao5@users.noreply.github.com>
2019-12-20 12:27:07 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Author: 25500
+# @Date:   2019-12-20 12:17:59
+# @Last Modified by:   25500
+# @Last Modified time: 2019-12-20 12:18:20
+"For loading module"
---
+++ setting.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# @Author: 25500
+# @Date:   2019-12-20 12:22:01
+# @Last Modified by:   25500
+# @Last Modified time: 2019-12-20 12:22:10
+# Common Setting for Networ awareness module.
+
+
+
+
+
+DISCOVERY_PERIOD = 10   			# For discovering topology.
+
+
+
+MONITOR_PERIOD = 10					# For monitoring traffic
+
+
+
+DELAY_DETECTING_PERIOD = 5			# For detecting link delay.
+
+
+
+TOSHOW = True						# For showing information in terminal
+
+	
+
+MAX_CAPACITY = 281474976710655L		# Max capacity of link
